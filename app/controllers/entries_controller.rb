453b981c6df206110d4c7b54bb1b49c9fb85bc73
Iryna Audzei <irynaaudzei@macbook-pro.mynet>
2020-09-21 15:36:52 +0100

    Added word length validation

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,19 +2,23 @@ class EntriesController < ApplicationController
   def index
     @entries = Entry.all
   end
-  
+
   def show
     @entry = Entry.find(params[:id])
   end
   
   def new
+    @entry = Entry.new
   end
 
   def create
     @entry = Entry.new(article_params)
 
-    @entry.save
-    redirect_to @entry
+    if @entry.save
+      redirect_to @entry
+    else
+      render "new"
+    end
   end
 
   private
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,3 @@
 class Entry < ApplicationRecord
+  validates :word, presence: true, length: { minimum: 1 }
 end

94cb29254a429c10d7ecaf543d332935979b5083
Iryna Audzei <irynaaudzei@macbook-pro.mynet>
2020-10-09 13:47:40 +0100

    Added bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,5 +13,12 @@ require("channels")
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
+require("bootstrap")
+import "bootstrap/dist/css/bootstrap"
+
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);

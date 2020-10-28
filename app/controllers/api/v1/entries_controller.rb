67225bd837b485fcde1181163683d0f2c9c3ffff
Iryna Audzei <irynaaudzei@macbook-pro.mynet>
2020-10-28 15:05:49 +0000

    Updated the Create action

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -6,6 +6,18 @@ class Api::V1::EntriesController < ApplicationController
   def show
   end
   def create
+    @entry = current_user.entries.build(entry_params)
+    if authorized?
+      respond_to do |format|
+        if @entry.save
+          format.json { render :show, status: :created, location: api_v1_entry_path(@entry) }
+        else
+          format.json { render json: entry.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      handle_unauthorised
+    end  
   end
   def update
   end
@@ -14,5 +26,9 @@ class Api::V1::EntriesController < ApplicationController
   private
     def set_entry
       @entry = Entry.find(params[:id])
+    end
+
+    def entry_params
+      params.require(:entry).permit(:word, :definition, :example)
     end
 end

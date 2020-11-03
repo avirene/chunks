7aaad18e8aa924a152c0772b132fcb891c9c66d3
Iryna Audzei <irynaaudzei@macbook-pro.mynet>
2020-11-03 15:06:14 +0000

    Trying to add a react app instead of the script tags in index.html.erb

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,8 +13,9 @@ require("channels")
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-require("bootstrap")
-import "bootstrap/dist/css/bootstrap"
+require("./components/ChunksApp");
+require("bootstrap");
+import "bootstrap/dist/css/bootstrap";
 
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)

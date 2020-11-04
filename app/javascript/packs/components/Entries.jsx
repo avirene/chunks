e03f2c4b4a39a0c7d590f48be813cda5fcb5d0b3
Iryna Audzei <avirene@live.com>
2020-11-04 12:53:49 +0000

    Added the Entries component

---
+++ app/javascript/packs/components/Entries.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+class Entries extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <>
+        <div className="table-responsive">
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Word</th>
+                <th scope="col">Definition</th>
+                <th scope="col">Example</th>
+                <th scope="col" className="text-right">
+                  Actions
+                </th>
+              </tr>
+            </thead>
+            <tbody>{this.props.children}</tbody>
+          </table>
+        </div>
+      </>
+    )
+  }
+}
+export default Entries

a0827049f08007cd3e1ebb035672d57242db1168
Iryna Audzei <avirene@live.com>
2020-11-09 12:21:48 +0000

    Added functionality for rendering the Entry and Entries Components

--- app/javascript/packs/components/ChunksApp.jsx
+++ app/javascript/packs/components/ChunksApp.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import Entries from "./Entries";
+import Entry from "./Entry";
 
 class ChunksApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      entries: []
+    };
+    this.getEntries = this.getEntries.bind(this);
+  }
+  componentDidMount() {
+    this.getEntries();
+  }
+  getEntries() {
+    axios
+    .get("/api/v1/entries")
+    .then(response => {
+      const entries = response.data;
+      this.setState({ entries });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+  
   render() {
-    return <p>ChunksApp</p>
+    return (
+      <Entries>
+        {this.state.entries.map(entry => (
+          <Entry key={entry.id} entry={entry} />
+        ))}
+      </Entries>
+    );
   }
 }
 
